051618e8c2a8afe8cbc9c05eefaf65b893abf184
Fahad <fahadsaadalzahrani8@gmail.com>
2023-08-16 22:35:47 +0300

    Finshed with comments

---
+++ index.js
@@ -0,0 +1,28 @@
+alert("(--------------------Task 1--------------------)") 
+{
+    for (let i = 1; i <= 10; i++){ // so in here we got asked to make a counter using for loop and make it count to 30 but stop at 10?
+        alert(i) // and we needed to use an alert instead of a console.log
+    }
+}
+alert("(--------------------Task 2--------------------)")
+{
+    for (let i = 10; i <= 20; i++){ // in here we were asked to make another counter using for loop but in this one yyou need to count till 20 but start from 10
+        alert(i) // and we needed to use an alert instead of a console.log
+    }
+}
+alert("(--------------------Task 3--------------------)")
+{
+    let i = 3 // in here we gave a var a value of 3
+    while(i >= 1){ // we needed to make a counter that counts backwards from 3 till 1 using while loops
+        alert(i) // and we needed to use an alert instead of a console.log
+        i -= 1 // in here it makes the counter count backward
+    }
+}
+alert("(--------------------Task 4--------------------)")
+{
+    let i = 0 // in here we gave a var a value of 0
+    do{ // this task was made by a while-do loop
+        alert(i) // and we needed to use an alert instead of a console.log
+        i++ // in this one we made it add one every time till it reaches 5
+    }while (i <= 5) // in here is the order we want the loop to do
+}
